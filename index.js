9c7bea49c9080af4aadae76bd6ee3905be3a9789
Abhinav Ajgaonkar <abhinav316@gmail.com>
2016-01-18 21:32:55 -0500

    Support punctuations at the end of hashtags

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ module.exports = function(text) {
 	var spaced = text.split(' '),
 		hashtagged = spaced.map(function(word) {
 			if (_.startsWith(word, '#')) {
-				return hashtag(word);
+				var words = _.words(word),
+					actualTag = words[0],
+					linkified = hashtag(actualTag),
+					replaced = word.replace(actualTag, linkified).replace('#', '');
+				return replaced;
 			} else {
 				return word;
 			}
--- test/index.js
+++ test/index.js
@@ -1,13 +1,18 @@
 var should = require('chai').should();
-var expect = require('chai').expect;
 
 var linkify = require('../index');
 
 describe('linkify', function() {
 
-	it('should linkify text', function() {
+	it('should linkify the simplest example with a single hashtag', function() {
 		var expected = 'Hello <a href="https://www.instagram.com/explore/tags/world">#world</a>';
 		var caption = 'Hello #world';
+		linkify(caption).should.equal(expected);
+	});
+
+	it('should linkify a caption with a single hashtag with a punctuation at the end', function() {
+		var expected = 'Hello <a href="https://www.instagram.com/explore/tags/world">#world</a>!';
+		var caption = 'Hello #world!';
 		linkify(caption).should.equal(expected);
 	});
 
